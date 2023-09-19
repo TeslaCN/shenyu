1f6e49014617487e77eba68330b6959b391e5b06
吴伟杰 <wuweijie@apache.org>
2023-09-19 21:17:15 +0800

    Migrate thymeleaf to spring6

--- shenyu-alert/src/main/java/org/apache/shenyu/alert/config/ThymeleafConfig.java
+++ shenyu-alert/src/main/java/org/apache/shenyu/alert/config/ThymeleafConfig.java
@@ -19,7 +19,7 @@ package org.apache.shenyu.alert.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 import java.nio.charset.StandardCharsets;
